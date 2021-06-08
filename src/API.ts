34069c916707ba61d09a414ee2aec8fd04f03ac6
gurbuzertunga <gurbuzertunga@gmail.com>
2021-06-08 20:29:35 +0300

    add user answers object

--- src/API.ts
+++ src/API.ts
@@ -21,7 +21,6 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
   const endPoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
 
   const data = await (await fetch(endPoint)).json();
-  console.log(data);
   return data.results.map((question: Question) => (
     {
       ...question,
--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import { useState } from 'react';
 import { fetchQuizQuestions } from './API';
 import QuestionCard from './components/QuestionCard';
 // TYPES
-import { Difficulty } from './API';
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  let [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
@@ -18,7 +25,8 @@ const App = () => {
   
   
   const startTrivia = async () => {
-
+    setLoading = true;
+    setGameOver = false;
   };
 
   const checkAnswer = (e:React.MouseEvent<HTMLButtonElement>) => {
