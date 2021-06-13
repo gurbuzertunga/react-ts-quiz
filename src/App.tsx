93f788477e8edd2cd8c6106025df5a273f48d870
gurbuzertunga <gurbuzertunga@gmail.com>
2021-06-13 20:57:50 +0300

    show scores

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ const App: React.FC = () => {
             <button className="start" onClick={startTrivia}>Start</button>
           )
           : null}
-      {!gameOver ? <p className="score">Score:</p> : null}
+      {!gameOver ? <p className="score">Score: {score}</p> : null}
       {loading && <p>Loading Questions...</p>}
       {!loading && !gameOver && (
         <QuestionCard
